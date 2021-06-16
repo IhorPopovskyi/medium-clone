2903b74a46c1192faf917d38ae444d584987a765
Ihor Popovskyi <57353154+IhorPopovskyi@users.noreply.github.com>
2021-06-16 20:37:02 +0300

    added feed toggler

--- src/app/global-feed/global-feed.module.ts
+++ src/app/global-feed/global-feed.module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { FeedModule } from '../shared/modules/feed/feed.module';
 import { BannerModule } from '../shared/modules/banner/banner.module';
 import { PopularTagsModule } from '../shared/modules/popular-tags/popular-tags.module';
+import { FeedTogglerModule } from '../shared/modules/feed-toggler/feed-toggler.module';
 
 const routes = [{ path: '', component: GlobalFeedComponent }];
 
 @NgModule({
-  imports: [CommonModule, FeedModule, BannerModule, RouterModule.forChild(routes), PopularTagsModule],
+  imports: [CommonModule, FeedModule, BannerModule, RouterModule.forChild(routes), PopularTagsModule, FeedTogglerModule],
   declarations: [GlobalFeedComponent],
 })
 export class GlobalFeedModule {}
